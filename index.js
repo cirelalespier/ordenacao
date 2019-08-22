a535272bbf82312e95e126c743bb56908c086afe
cirelalespier <lespier.cirela@gmail.com>
2019-08-21 22:50:59 -0300

    ordenação

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var numeros = [3, 1, 2];
-var i=0, aux;
+var numeros = [3, 1, 2, 5];
+var i = 0;
+var aux;
 
 while(i<numeros.length)
 {
 if (numeros[i]>numeros[i+1])
 {
-    aux == numeros[i];
-    numeros[i]==numeros[i+1];
-    numeros[i+1] == aux;
+    aux = numeros[i];
+    numeros[i] = numeros[i+1];
+    numeros[i+1] = aux;
 }
 i++;
 }
+
+console.log(numeros)
